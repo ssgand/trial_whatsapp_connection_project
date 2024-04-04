dd3a4b2d540451a43457bfb6208cc46bd06e3eb3
Sekamondo Steve Gandhi <sstevegandhi@gmail.com>
2024-04-04 19:19:17 +0200

    Made some changes

--- index.js
+++ index.js
@@ -25,10 +25,12 @@ app.get("/webhook", (req, res) => {
     let challenge = req.query["hub.challenge"];
   
     if (mode && token) {
-      if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+      if (mode == "subscribe" && token == process.env.VERIFY_TOKEN) {
         console.log("WEBHOOK_VERIFIED");
         res.status(200).send(challenge);
       } else {
+        console.log(mode);
+        console.log(token);
         res.sendStatus(403);
       }
     }
