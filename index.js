7c03fd9598cb365243f9fa6ca5183c2a8de263e7
Sekamondo Steve Gandhi <sstevegandhi@gmail.com>
2024-04-04 21:52:02 +0200

    Errors fixed

--- index.js
+++ index.js
@@ -29,11 +29,9 @@ app.get("/webhook", (req, res) => {
   
     if (mode && token) {
       if (mode === "subscribe" && token === verify_token) {
-        console.log(verify_token)
         console.log("WEBHOOK_VERIFIED");
         res.status(200).send(challenge);
       } else {
-        console.log(verify_token)
         res.sendStatus(403);
       }
     }
